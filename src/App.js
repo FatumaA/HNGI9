03a3560e3c989e185eb9153107b78b722f7e470a
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-11-01 17:16:18 +0300

    refactor: clean up app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import "./App.css";
+import btnData from "./data.json";
+import AnchorBtn from "./components/anchorBtn";
+import Profile from "./components/profile";
+import Footer from "./components/footer";
+import SocialSection from "./components/socialSection";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	const profInfo = {
+		id: "profile__img",
+		url: "profile.png",
+		btnText: "Annette Black",
+	};
+	return (
+		<>
+			<div className="App">
+				<Profile profInfo={profInfo} />
+				{btnData.map((data) => (
+					<AnchorBtn btnInfo={data} />
+				))}
+				<SocialSection />
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
