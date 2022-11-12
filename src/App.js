39c9b093e42782b442e169688ea9946c228c8fa6
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-11-12 14:47:00 +0300

    build: install react-router-dom and set it up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-	return <div></div>;
+	return (
+		<Routes>
+			<Route />
+			<Route />
+		</Routes>
+	);
 }
 
 export default App;
