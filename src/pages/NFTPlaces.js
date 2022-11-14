1ddeaea0f9b27d9173f4844e0fa8617df74ad595
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-11-14 17:30:14 +0300

    feat: add footer component to nft places page

--- src/pages/NFTPlaces.js
+++ src/pages/NFTPlaces.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Container } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const NFTPlaces = () => {
 	return (
-		<Container maxWidth="1200px" centerContent>
-			<Navbar />
-		</Container>
+		<>
+			<Container maxWidth="1200px" centerContent>
+				<Navbar />
+			</Container>
+			<div style={{ backgroundColor: "black" }}>
+				<Footer />
+			</div>
+		</>
 	);
 };
 
