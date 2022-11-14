74be1b2702e3e55749598efb993eec21c812f452
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-11-14 10:20:57 +0300

    feat: add skeleton for social proof section

--- src/sections/SocialProof.js
+++ src/sections/SocialProof.js
@@ -1,7 +1,34 @@
+import { HStack, Text, Image } from "@chakra-ui/react";
 import React from "react";
 
 const SocialProof = () => {
-	return <div>SocialProof</div>;
+	return (
+		<HStack
+			p="4"
+			align="stretch"
+			justify="space-between"
+			backgroundColor="#A02279"
+		>
+			<HStack>
+				<Image src={require("../images/mbtoken.png")} alt="mbtoken logo" />
+				<Text color="white" fontWeight="bold">
+					MBToken
+				</Text>
+			</HStack>
+			<HStack>
+				<Image src={require("../images/metamask.png")} alt="metamask logo" />
+				<Text color="white" fontWeight="bold">
+					METAMASK
+				</Text>
+			</HStack>
+			<HStack>
+				<Image src={require("../images/opensea.png")} alt="opensea logo" />
+				<Text color="white" fontWeight="bold">
+					OpenSea
+				</Text>
+			</HStack>
+		</HStack>
+	);
 };
 
 export default SocialProof;
