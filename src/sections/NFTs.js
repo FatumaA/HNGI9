2b8fcd2be348c9e62c6e49151d219e0a6cca9251
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-11-14 17:34:50 +0300

    fix: add whitespace btwn nft home and metainfo section

--- src/sections/NFTs.js
+++ src/sections/NFTs.js
@@ -4,7 +4,7 @@ import Card from "../components/Card";
 
 const NFTs = () => {
 	return (
-		<Container maxWidth="1200px">
+		<Container maxWidth="1200px" mb="20">
 			<Text fontSize="4xl" align="center" mt="40px" mb="40px" fontWeight="700">
 				Inspiration for your next adventure
 			</Text>
