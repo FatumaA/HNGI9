8472aed3c3e853f3307735bb8c58a6af3a43688d
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-11-15 21:55:54 +0300

    feat: add skeleton responsiveness on card sections

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,16 @@
 import React from "react";
 import {
+	Stack,
 	VStack,
 	Image,
 	Text,
 	Grid,
-	GridItem,
 	HStack,
 	Container,
+	SimpleGrid,
+	GridItem,
 } from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
 import homepic1 from "../images/homepic1.png";
 import homepic2 from "../images/homepic2.png";
 import homepic3 from "../images/homepic3.png";
@@ -16,9 +19,16 @@ import homepic5 from "../images/homepic5.png";
 import homepic6 from "../images/homepic6.png";
 import homepic7 from "../images/homepic7.png";
 import homepic8 from "../images/homepic8.png";
-import { StarIcon } from "@chakra-ui/icons";
+import nftpg9 from "../images/nftpg9.png";
+import nftpg10 from "../images/nftpg10.png";
+import nftpg11 from "../images/nftpg11.png";
+import nftpg12 from "../images/nftpg12.png";
+import nftpg13 from "../images/nftpg13.png";
+import nftpg14 from "../images/nftpg14.png";
+import nftpg15 from "../images/nftpg15.png";
+import nftpg16 from "../images/nftpg16.png";
 
-const Card = ({ pics }) => {
+const Card = ({ isHomePage = true }) => {
 	const p = [
 		homepic1,
 		homepic2,
@@ -29,26 +39,56 @@ const Card = ({ pics }) => {
 		homepic7,
 		homepic8,
 	];
-	return (
+	const nftpics = [
+		homepic1,
+		homepic2,
+		homepic3,
+		homepic4,
+		homepic5,
+		homepic6,
+		homepic7,
+		homepic8,
+		nftpg9,
+		nftpg10,
+		nftpg11,
+		nftpg12,
+		nftpg13,
+		nftpg14,
+		nftpg15,
+		nftpg16,
+	];
+	return isHomePage ? (
 		<Container maxWidth="1200px">
-			<Grid templateColumns="repeat(4, 1fr)" gap={6}>
-				{p.map((x) => (
+			<Grid
+				// columns={["1", "2", "4"]}
+				// width="100%"
+				// height="auto"
+				templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(4, 1fr)"]}
+				gap={6}
+			>
+				{p.map((x, i) => (
 					<>
-						<VStack
+						<Stack
+							w="fit-content"
 							alignItems="start"
 							p="10px"
 							border="1px solid #D7D7D7"
 							borderRadius="10px"
 						>
-							<Image src={x} alt={x} />
-							<HStack>
+							<Image
+								maxHeight={["150px", "220px", "250px"]}
+								key={i}
+								src={x}
+								alt={x}
+							/>
+							<Stack direction={["column", "row"]}>
 								<Text>Desert King</Text>
 								<Text fontWeight="bold">1MBT per night</Text>
-							</HStack>
-							<HStack>
+							</Stack>
+							<Stack direction={["column", "row"]}>
 								<Text>2345km away</Text>
-								<Text>Available for 2 weeks stay</Text>
-							</HStack>
+								<Text maxWidth="fit-content">Available for 2 weeks stay</Text>
+							</Stack>
 							<HStack>
 								<StarIcon color="pink.700"></StarIcon>
 								<StarIcon color="pink.700"></StarIcon>
@@ -56,7 +96,48 @@ const Card = ({ pics }) => {
 								<StarIcon color="pink.700"></StarIcon>
 								<StarIcon color="pink.700"></StarIcon>
 							</HStack>
-						</VStack>
+						</Stack>
+					</>
+				))}
+			</Grid>
+		</Container>
+	) : (
+		<Container maxWidth="1200px">
+			<Grid
+				templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(4, 1fr)"]}
+				gap={6}
+			>
+				{nftpics.map((np, i) => (
+					<>
+						<Stack
+							w="fit-content"
+							alignItems="start"
+							p="10px"
+							border="1px solid #D7D7D7"
+							borderRadius="10px"
+						>
+							<Image
+								maxHeight={["150px", "220px", "250px"]}
+								key={i}
+								src={np}
+								alt={np}
+							/>
+							<Stack direction={["column", "row"]}>
+								<Text>Desert King</Text>
+								<Text fontWeight="bold">1MBT per night</Text>
+							</Stack>
+							<Stack direction={["column", "row"]}>
+								<Text>2345km away</Text>
+								<Text maxWidth="fit-content">Available for 2 weeks stay</Text>
+							</Stack>
+							<HStack>
+								<StarIcon color="pink.700"></StarIcon>
+								<StarIcon color="pink.700"></StarIcon>
+								<StarIcon color="pink.700"></StarIcon>
+								<StarIcon color="pink.700"></StarIcon>
+								<StarIcon color="pink.700"></StarIcon>
+							</HStack>
+						</Stack>
 					</>
 				))}
 			</Grid>
--- src/sections/NFTs.js
+++ src/sections/NFTs.js
@@ -5,7 +5,13 @@ import Card from "../components/Card";
 const NFTs = () => {
 	return (
 		<Container maxWidth="1200px" mb="20" id="nfts">
-			<Text fontSize="4xl" align="center" mt="40px" mb="40px" fontWeight="700">
+			<Text
+				fontSize={["2xl", "3xl", "4xl"]}
+				align="center"
+				mt="40px"
+				mb="40px"
+				fontWeight="700"
+			>
 				Inspiration for your next adventure
 			</Text>
 			{/* Insert cards here */}
