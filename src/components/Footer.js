f21765dcdf472d526fe85e67e9ee1d329d49a97f
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-11-15 22:12:25 +0300

    feat: make footer resposive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,26 +12,29 @@ import {
 const Footer = () => {
 	return (
 		<Container maxWidth="1200px" color="white" pt="20" pb="10">
-			<Grid templateColumns="repeat(4, 1fr)" gap={24}>
+			<Grid
+				templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(4, 1fr)"]}
+				gap={["6", "10", "24"]}
+			>
 				<VStack align="start" mr="2.5">
 					<Image
 						maxHeight="8"
 						src={require("../images/logo-white.png")}
 						onClick={() => (window.location.href = "/")}
-						mb="20"
+						mb={["2", "4", "20"]}
 					/>
-					<HStack spacing={10}>
+					<HStack spacing={["2", "4", "10"]}>
 						<Image src={require("../images/fb-icon.png")} />
 						<Image src={require("../images/insta-icon.png")} />
 						<Image src={require("../images/twitter-icon.png")} />
 					</HStack>
-					<HStack pt="20">
+					<HStack pt={["2", "4", "20"]}>
 						<Image src={require("../images/copyright-icon.png")} />
 						<Text>2022 Metabnb</Text>
 					</HStack>
 				</VStack>
 				<VStack align="start">
-					<Text fontWeight="bold" id="community" mb="24px">
+					<Text fontWeight="bold" id="community" mb={["-1", "2", "8"]}>
 						Community
 					</Text>
 					<Text>NFT</Text>
@@ -40,7 +43,7 @@ const Footer = () => {
 					<Text>Discord</Text>
 				</VStack>
 				<VStack align="start">
-					<Text fontWeight="bold" mb="24px">
+					<Text fontWeight="bold" mb={["-1", "2", "8"]}>
 						Places
 					</Text>
 					<Text>Castle</Text>
@@ -49,7 +52,7 @@ const Footer = () => {
 					<Text>Learn more</Text>
 				</VStack>
 				<VStack align="start">
-					<Text fontWeight="bold" mb="24px">
+					<Text fontWeight="bold" mb={["-1", "2", "8"]}>
 						About us
 					</Text>
 					<Text>Road map</Text>
